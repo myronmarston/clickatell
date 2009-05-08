863ab0c48832e4c4101036f6a7bcef141f4ab1de
Myron Marston <myron.marston@gmail.com>
2009-05-08 14:25:22 -0700

    Fixed bug in response parser.  Integer strings that have leading zeroes get converted to integers using base 8 by YAML.load.  To fix this, I just removed leading zeroes from these strings before calling YAML.load.

--- lib/clickatell/response.rb
+++ lib/clickatell/response.rb
@@ -1,21 +1,27 @@
 require 'yaml'
 
 module Clickatell
-  
+
   # Used to parse HTTP responses returned from Clickatell API calls.
   class Response
 
     class << self
       PARSE_REGEX = /[A-Za-z0-9]+:.*?(?:(?=[A-Za-z0-9]+:)|$)/
-      
+
       # Returns the HTTP response body data as a hash.
       def parse(http_response)
         if http_response.body.scan(/ERR/).any?
           raise Clickatell::API::Error.parse(http_response.body)
         end
-        YAML.load(http_response.body.scan(PARSE_REGEX).join("\n"))
+
+        # YAML.load converts integer strings that have leading zeroes into integers
+        # using octal rather than decimal.  This isn't what we want, so we'll strip out any
+        # leading zeroes in numbers here.
+        response_fields = http_response.body.scan(PARSE_REGEX)
+        response_fields = response_fields.collect { |field| field.gsub(/\b0+(\d+)\b/, '\1') }
+        YAML.load(response_fields.join("\n"))
       end
-      
+
     end
 
   end
